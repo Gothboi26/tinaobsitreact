06a4f3156936d5960aacb35bff419900214e3aa3
Ain't No Way <tinaojanashley@gmail.com>
2023-11-24 15:56:10 +0800

    effects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <h1 className="name"> Tinao</h1>
+      <h1 className="title"> Implementing Css</h1>
+    </div>
+  );
 }
-
 export default App;
